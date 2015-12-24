797a686e90d4e1bf8a8e112f9b6a76e680fce060
JB <jeyboy@bigmir.net>
2015-12-24 02:47:56 +0200

    update presets list

--- parts/highligters/presets/presets_list.h
+++ parts/highligters/presets/presets_list.h
@@ -5,5 +5,13 @@
 #include "c_plus_preset.h"
 #include "sql_preset.h"
 #include "c_sharp_preset.h"
+#include "css_preset.h"
+#include "delphi_preset.h"
+#include "java_preset.h"
+#include "javascript_preset.h"
+#include "php_preset.h"
+#include "python_preset.h"
+#include "vb_preset.h"
+#include "xml_preset.h"
 
 #endif // PRESETS_LIST
