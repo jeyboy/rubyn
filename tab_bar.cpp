5a4e3a17aa74343d7d78b94943f2834aa1cec58d
JB <jeyboy1985@gmail.com>
2018-06-13 23:37:35 +0300

    temp

--- tab_bar.cpp
+++ tab_bar.cpp
@@ -1,7 +1,33 @@
 #include "tab_bar.h"
 
-TabBar::TabBar(QWidget * parent) : QTabBar(parent) {
+//* List of visible tabs
+//QRect r = tabBar->rect();
+//int numTabs = tabBar->count();
+//int firstVisible = 0;
+//int lastVisible = numTabs - 1;
 
+//while(firstVisible < numTabs && !r.intersects(tabBar->tabRect(firstVisible)))
+//    ++firstVisible;
+
+//while(lastVisible > 0 && !r.intersects(tabBar->tabRect(lastVisible)))
+//    --lastVisible;
+
+//qDebug() << firstVisible << lastVisible;
+
+//If you want only fully visible tabs switch intersects() to contains().
+
+TabBar::TabBar(QWidget * parent) : QTabBar(parent) {
+//    "QTabBar::scroller{width:85pix;}"
+//    setStyleSheet("QTabBar QToolButton::right-arrow {background-color: 'light blue';} QTabBar QToolButton::left-arrow {background-color: 'light blue';}");
+//    image: url(rightarrow.png);
+
+//    QTabBar::close-button {
+//        image: url(close.png)
+//        subcontrol-position: left;
+//    }
+//    QTabBar::close-button:hover {
+//        image: url(close-hover.png)
+//    }
 }
 
 void TabBar::tabLayoutChange() {
