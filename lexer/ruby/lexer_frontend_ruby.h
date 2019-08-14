57f94130325a283ab870605a7a05ed3087d6a75b
JB <jeyboy1985@gmail.com>
2019-08-15 00:49:23 +0300

    refactor

--- lexer/ruby/lexer_frontend_ruby.h
+++ lexer/ruby/lexer_frontend_ruby.h
@@ -44,6 +44,10 @@ namespace Ruby {
         bool parseRegexpGroup(LexerControl * state);
 
         void lexicate(LexerControl * control);
+
+        bool isCompleterInitiable(const LEXEM_TYPE & lex, const bool & at_end);
+        bool isCompleterContinuable(const LEXEM_TYPE & lex, const bool & /*at_end*/);
+        bool isCompleterReplaceable(const LEXEM_TYPE & lex, const bool & at_start);
     protected:
         int rubyLineState(BlockUserData * udata, const int & prev_user_state, const bool & override_status);
     public:
