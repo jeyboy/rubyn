ccb363dede1c467cc13c80eb2a6897fff4c80913
JB <jeyboy1985@gmail.com>
2019-12-15 02:34:17 +0200

    improve backspace key logic

--- editor/custom/custom_editor.cpp
+++ editor/custom/custom_editor.cpp
@@ -506,9 +506,22 @@ void Editor::customKeyPressEvent(QKeyEvent * e) {
 
             Cursor & cursor = _context -> _cursors[0];
 
-            cursor.block() -> removeText(cursor.posInBlock() - 1, 1);
+            if (cursor.posInBlock() == 0) {
+                IBlock * curr_block = cursor.block();
+                IBlock * prev_block = curr_block -> prev();
 
-            cursor.toPrevChar();
+                setTextCursor(
+                    Cursor(_document, prev_block, prev_block -> text().length())
+                );
+
+                prev_block -> appendText(curr_block -> text().toUtf8());
+                curr_block -> remove();
+            } else {
+                cursor.block() -> removeText(cursor.posInBlock() - 1, 1);
+
+                cursor.toPrevChar();
+            }
+
             update();
         break;}
 
